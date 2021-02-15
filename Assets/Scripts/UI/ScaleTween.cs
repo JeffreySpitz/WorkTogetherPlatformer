3bf4f5649883855de57895f72561ec3c4168f235
JeffreySpitz <jeffreyspitz99@berkeley.edu>
2021-02-14 22:07:20 -0800

    Practicing with menus

---
+++ Assets/Scripts/UI/ScaleTween.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTween : MonoBehaviour
+{
+    private float scale_x;
+    private float scale_y;
+
+    public float scale_multiplier = 1.2f;
+    public float scale_time = 0.5f;
+
+    private void Start()
+    {
+        scale_x = transform.localScale.x;
+        scale_y = transform.localScale.y;
+    }
+
+    public void ScaleUp()
+    {
+        LeanTween.scale(gameObject, new Vector2(scale_x * scale_multiplier, scale_y * scale_multiplier), scale_time);
+    }
+
+    public void ScaleDown()
+    {
+        LeanTween.scale(gameObject, new Vector2(scale_x, scale_y), scale_time);
+    }
+
+}
