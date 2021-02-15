fe3d687d0d42f925e12654d5ab67572aaae81927
JeffreySpitz <jeffreyspitz99@berkeley.edu>
2021-02-15 13:08:55 -0800

    tweaked menu

--- Assets/Scripts/Core/MainMenuManager.cs
+++ Assets/Scripts/Core/MainMenuManager.cs
@@ -1,23 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-
     public void Play()
     {
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 }
