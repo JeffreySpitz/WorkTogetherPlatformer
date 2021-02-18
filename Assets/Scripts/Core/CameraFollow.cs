53d848f4e5ee542921b6337e8b3af69b3d1a4672
JeffreySpitz <jeffreyspitz99@berkeley.edu>
2021-02-17 22:17:22 -0800

    Adding camera follow script

---
+++ Assets/Scripts/Core/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smooth_speed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        // Late update so target have already conducted its movement
+        Vector3 desired_position = target.position + offset;
+        float distance = Vector3.Distance(desired_position, transform.position);
+        Vector3 smoothed_position = Vector3.Lerp(transform.position, desired_position, smooth_speed);
+        transform.position = smoothed_position;
+
+        transform.LookAt(target);
+    }
+}
